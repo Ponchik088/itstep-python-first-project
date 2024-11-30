bcf446fe7ffd359656f95122206d495b5567c7fa
alimuradov <ponchik088@gmail.com>
2024-11-30 11:34:35 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import tkinter as tk
-monster1 = tk.PhotoImage(file =r"C:\Gfg\circle.png")
 
 window = tk.Tk()
 window.geometry("500x700")
 window.resizable(False, False)
 window.title("MegaClicker")
 
-
+monster1 = tk.PhotoImage(file =r".\img\Monster1.png")
 
 greeting = tk.Label(text="Hello, Tkinter", fg="#ff0000")
 greeting.pack()
